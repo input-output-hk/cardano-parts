48cf8bcfd41804d3605d23847703e9b172efa643
John Lotoski <john.lotoski@iohk.io>
2023-09-07 18:03:23 -0500

    fix: accept flake config warns wrt daemon required option

--- flake.nix
+++ flake.nix
@@ -117,7 +117,6 @@
     });
 
   nixConfig = {
-    extra-experimental-features = ["fetch-closure"];
     extra-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
     allow-import-from-derivation = "true";
