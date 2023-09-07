864d3d9b2c47a0b3d6f2a0d953a4dd650e3bd823
Samuel Leathers <samuel.leathers@iohk.io>
2023-09-07 14:33:42 -0400

    remove experimental features override

--- flake.nix
+++ flake.nix
@@ -114,7 +114,6 @@
     });
 
   nixConfig = {
-    extra-experimental-features = ["fetch-closure"];
     extra-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
     allow-import-from-derivation = "true";
