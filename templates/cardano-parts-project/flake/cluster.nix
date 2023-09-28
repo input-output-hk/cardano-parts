bf031b875e197778638dbf2ffa85635e3f19138b
John Lotoski <john.lotoski@iohk.io>
2023-09-28 16:52:23 -0500

    tmpl: update cluster and colmena machine defns

--- templates/cardano-parts-project/flake/cluster.nix
+++ templates/cardano-parts-project/flake/cluster.nix
@@ -5,23 +5,37 @@
   #
   # Define cluster-wide configuration.
   # This has to evaluate fast and is imported in various places.
-  flake.cardano-parts.cluster.infra.aws = {
-    # orgId = "UPDATE_ME";
-    # region = "eu-central-1";
-    # profile = "UPDATE_ME";
+  flake.cardano-parts.cluster = rec {
+    infra.aws = {
+      # orgId = "UPDATE_ME";
+      # region = "eu-central-1";
+      # profile = "UPDATE_ME";
 
-    # A list of all regions in use, with a bool indicating inUse status.
-    # Set a region to false to set its count to 0 in terraform.
-    # After terraform applying once the line can be removed.
-    #
-    # regions = {
-    #   eu-central-1 = true;
-    # };
+      # A list of all regions in use, with a bool indicating inUse status.
+      # Set a region to false to set its count to 0 in terraform.
+      # After terraform applying once the line can be removed.
+      #
+      # regions = {
+      #   eu-central-1 = true;
+      # };
 
-    # domain = "UPDATE_ME";
+      # domain = "UPDATE_ME";
 
-    # Preset defaults matched to default terraform rain infra; change if desired:
-    # kms = "arn:aws:kms:${region}:${orgId}:alias/kmsKey";
-    # bucketName = "${profile}-terraform";
+      # Preset defaults matched to default terraform rain infra; change if desired:
+      # kms = "arn:aws:kms:${region}:${orgId}:alias/kmsKey";
+      # bucketName = "${profile}-terraform";
+    };
+
+    # If using grafana cloud stack based monitoring.
+    # infra.grafana.stackName = "UPDATE_ME";
+
+    # For defining deployment groups with varying configuration.  Adjust as needed.
+    group = {
+      preview1 = {
+        groupPrefix = "preview1-";
+        meta.environmentName = "preview";
+        groupRelayMultivalueDns = "preview.${infra.aws.domain}";
+      };
+    };
   };
 }
--- templates/cardano-parts-project/flake/colmena.nix
+++ templates/cardano-parts-project/flake/colmena.nix
@@ -9,25 +9,48 @@ in {
   flake.colmena = let
     # Region defs:
     eu-central-1.aws.region = "eu-central-1";
+    eu-west-1.aws.region = "eu-west-1";
+    us-east-2.aws.region = "us-east-2";
 
     # Instance defs:
     t3a-small.aws.instance.instance_type = "t3a.small";
 
-    # OS defs:
-    nixos-23-05.system.stateVersion = "23.05";
+    # Helper fns:
+    ebs = size: {aws.instance.root_block_device.volume_size = lib.mkDefault size;};
 
     # Helper defs:
     # delete.aws.instance.count = 0;
 
-    # Helper fns:
-    ebs = size: {aws.instance.root_block_device.volume_size = lib.mkDefault size;};
+    # Cardano group assignments:
+    preview1 = {cardano-parts.cluster.group = config.flake.cardano-parts.cluster.group.preview1;};
+
+    # Cardano-node modules for group deployment
+    node = {
+      imports = [
+        # Base cardano-node service
+        config.flake.cardano-parts.cluster.group.default.meta.cardano-node-service
+
+        # Config for cardano-node group deployments
+        inputs.cardano-parts.nixosModules.module-cardano-node-group
+
+        # Default group deployment topology
+        topoSimple
+      ];
+    };
+
+    # Profiles
+    topoSimple = {imports = [inputs.cardano-parts.nixosModules.profile-topology-simple];};
+    # pre = {imports = [inputs.cardano-parts.nixosModules.profile-pre-release];};
+
+    # Roles
+    rel = {imports = [inputs.cardano-parts.nixosModules.role-relay];};
+    bp = {imports = [inputs.cardano-parts.nixosModules.role-block-producer];};
   in {
     meta = {
       nixpkgs = import inputs.nixpkgs {
         system = "x86_64-linux";
       };
 
-      # Make node spec info available to nixosModules
       nodeSpecialArgs =
         lib.foldl'
         (acc: node: let
@@ -37,7 +60,7 @@ in {
             ${node} = {
               nodeResources = {
                 inherit
-                  (config.flake.cardano-parts.aws-ec2.spec.${instanceType node})
+                  (config.flake.cardano-parts.aws.ec2.spec.${instanceType node})
                   provider
                   coreCount
                   cpuCount
@@ -52,13 +75,17 @@ in {
     };
 
     defaults.imports = [
-      inputs.cardano-parts.nixosModules.aws-ec2
-      inputs.cardano-parts.nixosModules.basic
-      inputs.cardano-parts.nixosModules.cardano-parts
+      inputs.cardano-parts.nixosModules.module-aws-ec2
+      inputs.cardano-parts.nixosModules.module-cardano-parts
+      inputs.cardano-parts.nixosModules.profile-basic
+      inputs.cardano-parts.nixosModules.profile-common
+      inputs.cardano-parts.nixosModules.profile-grafana-agent
       nixosModules.common
-      nixos-23-05
     ];
 
-    example-machine = {imports = [eu-central-1 t3a-small (ebs 40)];};
+    preview1-bp-a-1 = {imports = [eu-central-1 t3a-small (ebs 40) preview1 node bp];};
+    preview1-rel-a-1 = {imports = [eu-central-1 t3a-small (ebs 40) preview1 node rel];};
+    preview1-rel-b-1 = {imports = [eu-west-1 t3a-small (ebs 40) preview1 node rel];};
+    preview1-rel-c-1 = {imports = [us-east-2 t3a-small (ebs 40) preview1 node rel];};
   };
 }
