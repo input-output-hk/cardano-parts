0911b54a35e748dd26d5dda2032e920192c54b7a
John Lotoski <john.lotoski@iohk.io>
2023-10-20 10:16:18 -0500

    imp: add a meta description to pre-push package

--- perSystem/packages/pre-push.nix
+++ perSystem/packages/pre-push.nix
@@ -6,33 +6,37 @@
     config,
     ...
   }: {
-    packages.pre-push = pkgs.writeShellApplication {
-      name = "pre-push";
-      runtimeInputs = with pkgs; [coreutils git gnugrep];
-      text = ''
-        tput bold # bold
-        tput setaf 5 # magenta
-        echo >&2 'To skip, run git push with --no-verify.'
-        tput sgr0 # reset
+    packages.pre-push =
+      lib.recursiveUpdate (pkgs.writeShellApplication {
+        name = "pre-push";
+        runtimeInputs = with pkgs; [coreutils git gnugrep];
+        text = ''
+          tput bold # bold
+          tput setaf 5 # magenta
+          echo >&2 'To skip, run git push with --no-verify.'
+          tput sgr0 # reset
 
-        SECRETS_DIR="$(git rev-parse --show-toplevel)"/secrets
-        if [ -d "$SECRETS_DIR" ]; then
-          if [ "$(grep -rL '"data": "ENC' "$SECRETS_DIR" | wc -l)" != "0" ]; then
-            echo
-            echo "The following secrets/ files appear to be un-encrypted or not binary encrypted:"
-            grep -rL '"data": "ENC' "$SECRETS_DIR"
-            exit 1
+          SECRETS_DIR="$(git rev-parse --show-toplevel)"/secrets
+          if [ -d "$SECRETS_DIR" ]; then
+            if [ "$(grep -rL '"data": "ENC' "$SECRETS_DIR" | wc -l)" != "0" ]; then
+              echo
+              echo "The following secrets/ files appear to be un-encrypted or not binary encrypted:"
+              grep -rL '"data": "ENC' "$SECRETS_DIR"
+              exit 1
+            fi
           fi
-        fi
 
-        declare -a checks
-        for check in ${lib.escapeShellArgs (builtins.attrNames config.checks)}; do
-          checks+=(.#checks.${lib.escapeShellArg system}."$check")
-        done
+          declare -a checks
+          for check in ${lib.escapeShellArgs (builtins.attrNames config.checks)}; do
+            checks+=(.#checks.${lib.escapeShellArg system}."$check")
+          done
 
-        set -x
-        nix build "''${checks[@]}"
-      '';
-    };
+          set -x
+          nix build "''${checks[@]}"
+        '';
+      }) {
+        # Until nixpkgs 23.11 where meta is a parameter of writeShellApplication
+        meta.description = "A pre-push repo check for required secrets encryption, linting and formatting";
+      };
   };
 }
