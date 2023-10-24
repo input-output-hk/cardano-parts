122c51df5066856ed9e41a6dbdd5631b2467dd3d
John Lotoski <john.lotoski@iohk.io>
2023-10-24 13:32:15 -0500

    bump: cardano-db-sync-ng / schema to sancho-2-1-0

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     cardano-db-sync-schema-ng = {
-      url = "github:input-output-hk/cardano-db-sync/sancho-2-0-1";
+      url = "github:input-output-hk/cardano-db-sync/sancho-2-1-0";
       flake = false;
     };
 
