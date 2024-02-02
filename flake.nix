34557fa86cd51debbe1f9acfae17cf6ddc0fe55f
John Lotoski <john.lotoski@iohk.io>
2024-02-02 09:33:38 -0600

    bump: iohk-nix for mithril client pr merge

--- flake.nix
+++ flake.nix
@@ -28,8 +28,8 @@
     capkgs.url = "github:input-output-hk/capkgs";
     empty-flake.url = "github:input-output-hk/empty-flake";
     haskell-nix.url = "github:input-output-hk/haskell.nix";
-    iohk-nix.url = "github:input-output-hk/iohk-nix/mithril-client";
-    iohk-nix-ng.url = "github:input-output-hk/iohk-nix/mithril-client";
+    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    iohk-nix-ng.url = "github:input-output-hk/iohk-nix";
 
     # Cardano related inputs required for service config
     # Services offered from the nixosModules of this repo are directly assigned to
