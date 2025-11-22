0f36e29073a5c285ef8359afc7686db451acc636
John Lotoski <john.lotoski@iohk.io>
2025-11-22 03:48:03 -0600

    bump: capkgs for node release 10.5.3

--- flakeModules/pkgs.nix
+++ flakeModules/pkgs.nix
@@ -478,7 +478,7 @@ in
           mithril-release = "input-output-hk-mithril-2543-1-hotfix-5d5571e";
           mithril-pre-release = "input-output-hk-mithril-unstable-2943337";
 
-          node-release = pkg: caPkgs."${pkg}-input-output-hk-cardano-node-10-5-2-1ec98e9";
+          node-release = pkg: caPkgs."${pkg}-input-output-hk-cardano-node-10-5-3-6c034ec";
           # node-release = pkg: localFlake.inputs.cardano-node-10-5-2.packages.x86_64-linux.${pkg};
 
           node-pre-release = pkg: caPkgs."${pkg}-input-output-hk-cardano-node-10-6-0-f5ac0eb";
