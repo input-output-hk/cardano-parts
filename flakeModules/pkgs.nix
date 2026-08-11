9e932dc4a0a0e1d81c7574a5ea5115a32fd15f3f
John Lotoski <john.lotoski@iohk.io>
2026-08-11 00:48:53 -0500

    pkgs: update faucet-ng for leios w32

--- flakeModules/pkgs.nix
+++ flakeModules/pkgs.nix
@@ -471,8 +471,8 @@ in
           faucet = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-11-0-631bb64";
           # faucet = localFlake.inputs.cardano-faucet.packages.x86_64-linux."cardano-faucet:exe:cardano-faucet";
 
-          faucet-ng = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-leios-prototype-w30-ea2fa54";
-          # faucet-ng = localFlake.inputs.cardano-faucet.packages.x86_64-linux."cardano-faucet:exe:cardano-faucet";
+          # faucet-ng = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-leios-prototype-w30-ea2fa54";
+          faucet-ng = localFlake.inputs.cardano-faucet.packages.x86_64-linux."cardano-faucet:exe:cardano-faucet";
 
           # metadata-pkg = pkg: caPkgs."${pkg}-input-output-hk-offchain-metadata-tools-ops-1-0-0-f406c6d";
           metadata-pkg = pkg: localFlake.inputs.cardano-metadata-service.packages.${system}.${pkg};
