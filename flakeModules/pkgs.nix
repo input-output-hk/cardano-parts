4aaa713721d1b1ff2c4ec0d4eb35977e7a154500
John Lotoski <john.lotoski@iohk.io>
2026-08-18 02:21:45 -0500

    bump: capkgs, node 11.1.0 rc pins, pkgs: update faucet[-ng]

--- flakeModules/pkgs.nix
+++ flakeModules/pkgs.nix
@@ -468,10 +468,10 @@ in
           dbsync-release = "input-output-hk-cardano-db-sync-13-7-2-1-db8cdf6";
           dbsync-pre-release = "input-output-hk-cardano-db-sync-13-7-2-1-db8cdf6";
 
-          faucet = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-11-0-631bb64";
+          faucet = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-11-0a-829b8de";
           # faucet = localFlake.inputs.cardano-faucet.packages.x86_64-linux."cardano-faucet:exe:cardano-faucet";
 
-          faucet-ng = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-leios-prototype-w32-1dba45c";
+          faucet-ng = caPkgs."\"cardano-faucet:exe:cardano-faucet\"-input-output-hk-cardano-faucet-leios-prototype-w32a-6c7d52f";
           # faucet-ng = localFlake.inputs.cardano-faucet.packages.x86_64-linux."cardano-faucet:exe:cardano-faucet";
 
           metadata-pkg = pkg: caPkgs."${pkg}-input-output-hk-offchain-metadata-tools-v0-5-0-0-91eba72";
